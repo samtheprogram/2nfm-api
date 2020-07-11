448f9a96ed2d68c43f450b8def37dfc06e473534
Sam Moore <sam@paradigmpost.com>
2020-07-11 06:33:32 +0000

    chore(get-values-from-config-json): do not hand-hold editors of config.json:
    - overwrite any defaults if they are present whatsoever in config.json
    - do not support using strings for number or boolean values
    
    This greatly simplifies the logic of this file to (a) verifying the JSON exists and
    (b) running Object.assign using the defaults and the contents of config.json.

--- node_scripts/get-values-from-config-json.js
+++ node_scripts/get-values-from-config-json.js
@@ -3,11 +3,10 @@
 // Documentation  - github.com/muaz-khan/RTCMultiConnection
 
 function getValues(param) {
-    var path = require('path');
     var fs = require('fs');
     var getJsonFile = require('./getJsonFile.js');
 
-    var result = {
+    var DEFAULTS = {
         socketURL: '/',
         dirPath: null,
         homePage: '/demos/index.html',
@@ -25,83 +24,12 @@ function getValues(param) {
         adminPassword: null
     };
 
-
     if (!fs.existsSync(param.config)) {
         console.log('File does not exist', param.config);
-        return result;
+        return Object.assign({}, DEFAULTS);
     }
 
-    var config = getJsonFile(param.config);
-
-    ['sslKey', 'sslCert', 'sslCabundle'].forEach(function(key) {
-        if (!config[key] || config[key].toString().length == 0) {
-            return;
-        }
-
-        if (config[key].indexOf('/path/to/') === -1) {
-            if (key === 'sslKey') {
-                result.sslKey = config['sslKey'];
-            }
-
-            if (key === 'sslCert') {
-                result.sslCert = config['sslCert'];
-            }
-
-            if (key === 'sslCabundle') {
-                result.sslCabundle = config['sslCabundle'];
-            }
-        }
-    });
-
-    if ((config.port || '').toString() !== '9001') {
-        result.port = (config.port || '').toString();
-    }
-
-    if ((config.autoRebootServerOnFailure || '').toString() === 'true') {
-        result.autoRebootServerOnFailure = true;
-    }
-
-    if ((config.isUseHTTPs || '').toString() === 'true') {
-        result.isUseHTTPs = true;
-    }
-
-    if ((config.enableLogs || '').toString() === 'true') {
-        result.enableLogs = true;
-    }
-
-    if ((config.socketURL || '').toString().length) {
-        result.socketURL = (config.socketURL || '').toString();
-    }
-
-    if ((config.dirPath || '').toString().length) {
-        result.dirPath = (config.dirPath || '').toString();
-    }
-
-    if ((config.homePage || '').toString().length) {
-        result.homePage = (config.homePage || '').toString();
-    }
-
-    if ((config.socketMessageEvent || '').toString().length) {
-        result.socketMessageEvent = (config.socketMessageEvent || '').toString();
-    }
-
-    if ((config.socketCustomEvent || '').toString().length) {
-        result.socketCustomEvent = (config.socketCustomEvent || '').toString();
-    }
-
-    if ((config.enableAdmin || '').toString() === 'true') {
-        result.enableAdmin = true;
-    }
-
-    if ((config.adminUserName || '').toString().length) {
-        result.adminUserName = (config.adminUserName || '').toString();
-    }
-
-    if ((config.adminPassword || '').toString().length) {
-        result.adminPassword = (config.adminPassword || '').toString();
-    }
-
-    return result;
+    return Object.assign({}, DEFAULTS, getJsonFile(param.config));
 }
 
 module.exports = exports = getValues;
